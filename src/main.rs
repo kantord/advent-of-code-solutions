b4ab5205ce7d708b4eb4680e0b03378fcbd58c73
Daniel Kantor <git@daniel-kantor.com>
2023-11-22 14:38:10 +0100

    set up basic runner for solutions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod year2022;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    match args
+        .get(1)
+        .as_deref()
+        .expect("specify a valid year and day!")
+        .as_str()
+    {
+        "2022" => year2022::run(args.get(2)),
+        _ => println!("Please specify a valid year and day (e.g., 2021 day01)"),
+    }
 }
---
+++ src/year2022/day01.rs
@@ -0,0 +1,3 @@
+pub fn run() {
+    println!("Hello, World! This is Day 1 of Advent of Code!");
+}
